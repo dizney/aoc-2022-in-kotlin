b8df162b2bed0f225208185477888a1a6d99c403
Sidney Beekhoven <903673+dizney@users.noreply.github.com>
2022-12-17 11:06:31 +0100

    feat: day 16 part 1

---
+++ src/Day16.kt
@@ -0,0 +1,116 @@
+import java.util.*
+
+object Day16 {
+    const val EXPECTED_PART1_CHECK_ANSWER = 1651
+    const val EXPECTED_PART2_CHECK_ANSWER = 1
+
+    const val RUNTIME_MINS = 30
+}
+
+data class Valve(val name: String, val flowRate: Int) {
+    private var _accessibleValves: Set<Valve> = emptySet()
+
+    val accessibleValves
+        get() = _accessibleValves
+
+    fun addAccessibleValve(valve: Valve) {
+        _accessibleValves += valve
+    }
+}
+
+fun main() {
+
+    fun List<String>.parseValves(): MutableMap<String, Valve> {
+        val paths = mutableMapOf<String, List<String>>()
+        val valves = mutableMapOf<String, Valve>()
+        forEach { valveString ->
+            val (name, flowRate, accessibleValves) = Regex(
+                "Valve (.+) has flow rate=(\\d+); tunnels? leads? to valves? (.*)"
+            )
+                .matchEntire(valveString)?.destructured ?: error("Valve line not matched")
+            if (valves.contains(name)) error("Duplicate valve name detected")
+            paths[name] = accessibleValves.split(',').map(String::trim)
+            valves[name] = Valve(name, flowRate.toInt())
+        }
+        paths.forEach { (name, accessibleValves) ->
+            accessibleValves.forEach { accessibleValveName ->
+                valves[name]?.addAccessibleValve(
+                    valves[accessibleValveName] ?: error("Could not find accessible valve")
+                )
+            }
+        }
+        return valves
+    }
+
+    fun shortestPathDistance(start: Valve, dest: Valve): Int {
+        val queue = LinkedList<Pair<Valve, Int>>()
+        val visited = mutableSetOf<Valve>()
+
+        queue.add(start to 0)
+        visited.add(start)
+
+        while (queue.isNotEmpty()) {
+            val (valve, distance) = queue.remove()
+            if (valve == dest) {
+                return distance
+            }
+
+            for (accessibleValve in valve.accessibleValves) {
+                if (!visited.contains(accessibleValve)) {
+                    queue.add(accessibleValve to distance + 1)
+                    visited.add(accessibleValve)
+                }
+            }
+        }
+        error("No shortest path found for $start to $dest")
+    }
+
+    fun releasedPressure(
+        startValve: Valve,
+        distances: Map<Valve, Map<Valve, Int>>,
+        openers: Int = 1,
+        opened: List<Valve> = emptyList(),
+        releasedPressure: Int = 0,
+        minutesRemaining: Int = Day16.RUNTIME_MINS
+    ): Int {
+        return distances[startValve]!!.filter { it.key !in opened }.maxOfOrNull { (nextValve, distance) ->
+            val newMinutesRemaining = minutesRemaining - distance - 1
+            if (newMinutesRemaining > 0) {
+                releasedPressure(
+                    nextValve,
+                    distances,
+                    openers,
+                    opened = opened + nextValve,
+                    releasedPressure = releasedPressure + nextValve.flowRate * newMinutesRemaining,
+                    minutesRemaining = newMinutesRemaining
+                )
+            } else releasedPressure
+        } ?: releasedPressure
+    }
+
+
+    fun part1(input: List<String>, startValveName: String): Int {
+        val valvesMap = input.parseValves()
+
+        val distances = valvesMap.values.associateWith { startValve ->
+            valvesMap.values.filter { it.flowRate > 0 }.associateWith { destValve ->
+                shortestPathDistance(startValve, destValve)
+            }
+        }
+
+        return releasedPressure(valvesMap[startValveName]!!, distances)
+    }
+
+    fun part2(input: List<String>): Int {
+        return 1
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day16_test")
+    check(part1(testInput, "AA") == Day16.EXPECTED_PART1_CHECK_ANSWER) { "Part 1 failed" }
+    check(part2(testInput) == Day16.EXPECTED_PART2_CHECK_ANSWER) { "Part 2 failed" }
+
+    val input = readInput("Day16")
+    println(part1(input, "AA"))
+    println(part2(input))
+}
